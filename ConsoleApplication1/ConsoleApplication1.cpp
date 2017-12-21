ec75ec0f4b7cff6a07dd3d188d5fc0b1ce52ee4b
Lenovo <Lenovo@DESKTOP-80UM70A>
2017-12-21 23:10:03 +0200

    k

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,19 @@
+// ConsoleApplication1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#pragma warning (disable: 4996)
+using namespace std;
+
+int main()
+{	
+	setlocale(LC_ALL, "rus");
+	char A[16];
+	
+
+    return 0;
+}
+
--- ConsoleApplication23/ConsoleApplication23.cpp
+++ ConsoleApplication23/ConsoleApplication23.cpp
@@ -2,10 +2,74 @@
 //
 
 #include "stdafx.h"
-
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#pragma warning(disable: 4996)
+using namespace std;
 
 int main()
 {
+	setlocale(LC_ALL, "rus");
+
+	FILE *fp;
+	char A1[16];
+	scanf("%s", A1);
+	long double A[20][5] = { {0},{0},{0} };
+	for (int i = 0; i < 20; i++)
+		for (int j = 0; j < 5; j++)
+			A[i][j] = 10 * i + j;
+
+	for (int i = 0; i < 20; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			cout << i << "." << j << ") " <<  A[i][j] << "\t\t";
+		}
+		cout << "\n\n";
+	}
+	fp = fopen(A1, "wt");
+	if (fp == NULL)
+	{
+		cout << "Error!";
+		exit(0);
+	}
+
+	for (int i = 0; i < 20; i++)
+	{
+		fprintf(fp, "\n %2d)", i + 1);
+		for (int j = 0; j < 5; j++)
+		{
+			fprintf(fp, " %3.0Lf", A[i][j]);
+		}
+	}
+	fclose(fp);
+
+	char A2[16];
+	scanf("%s", A2);
+	fp = fopen(A2, "wb");
+	if (fp == NULL)
+	{
+		cout << "Error!";
+		exit(0);
+	}
+
+
+	fwrite(&A, sizeof(double), 100, fp);	
+	fclose(fp);
+	fp = fopen(A2, "rb");
+	fread(&A, sizeof(double), 100, fp);
+
+	for (int i = 0; i < 20; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			cout << i << "." << j << ") " << A[i][j] << "\t\t";
+		}
+		cout << "\n\n";
+	}
+	fclose(fp);
+
     return 0;
 }
 
